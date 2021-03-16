c76ed60be3f0d0894df50a8f0cfffaa77cf485f8
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-16 16:58:30 +0300

    gett user age and give feedback

--- name.rb
+++ name.rb
@@ -2,4 +2,12 @@
 
 puts "What is your name?"
 name = gets.chomp
-puts "How are you doing " + name + "!"
+puts "How are you doing " + name + "!\n"
+
+#Getting user age and giving feedback
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In ten years you'll be "
+puts age + 10
+
+

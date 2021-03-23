0574e63c47c8cc1222e631ec3ec83fe7c5173e32
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-23 13:33:37 +0300

    yield in methods

--- blockNprocs.rb
+++ blockNprocs.rb
@@ -15,3 +15,12 @@ fibb = [5, 8, 2, 7, 11, 32, 45, 67]
 doubled_fibb = []
 doubled_fibb = fibb.collect {|integer| integer * 2}
 puts doubled_fibb
+
+
+# yield
+
+def doubler(number)
+  yield (number)
+end
+
+doubler(76) {|x| puts x * 5}

f77fff249d2a23fccd0c669591f436b6d6549d03
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-26 15:40:52 +0300

    iterate through arr and print elements

---
+++ exercisec.rb
@@ -0,0 +1,15 @@
+# 1 print numbers in the array
+
+arr = [1, 2, 3, 4, 5, 6, 7, 9, 10]
+arr.each {|integer| puts integer}
+
+puts "\n"
+
+# 2 print numbers in the array > 5
+
+arr.each do |integer|
+ if integer > 5
+    puts integer
+  end
+end
+

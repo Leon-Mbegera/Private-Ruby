3d698f2232ea4951c2de0f86e770c4c181719182
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-26 15:34:06 +0300

    inject method with only block

---
+++ customs_methods.rb
@@ -0,0 +1,34 @@
+def custom_each(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+end
+
+  
+names = ["Kariithi", "Kuria", "Ombuna", "Mwiti"]
+ages = [21, 17, 20, 22]
+
+  custom_each(names) do |name|
+    puts "The length of #{name} is #{name.length}"
+  end
+
+  puts "\n"
+
+# def my_each
+#   return to_enum unless block_given?
+
+#   length.times { |i| yield(self[i]) }
+# end
+
+# puts ([5, 3, 9, 2]).my_each {|i| i}
+
+puts "\n"
+
+
+# inject with only a block
+
+puts [4, 6, 2, 9].inject {|acc, ele| acc * ele}
+
+

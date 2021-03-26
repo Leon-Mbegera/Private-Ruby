edc54d03f68a086fd9e859b752eee52e2b4ecef3
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-26 16:57:17 +0300

    append and prepend extra element

--- exercisec.rb
+++ exercisec.rb
@@ -1,11 +1,11 @@
-# 1 print numbers in the array
+#1 print numbers in the array
 
 arr = [1, 2, 3, 4, 5, 6, 7, 9, 10]
 arr.each {|integer| puts integer}
 
 puts "\n"
 
-# 2 print numbers in the array > 5
+#2 print numbers in the array > 5
 
 arr.each do |integer|
  if integer > 5
@@ -13,6 +13,22 @@ arr.each do |integer|
   end
 end
 
-# 3 use select method to print odd integers
+puts "\n"
+
+#3 use select method to print odd integers
 
 arr.select {|integer| puts integer if integer.odd?}
+
+
+#4 append / prepend an element into an array
+
+arr.push(11)
+print arr
+
+puts "\n"
+
+arr.unshift(0)
+print arr
+
+
+puts "\n"

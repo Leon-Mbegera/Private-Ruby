fdb28c26872d9e60bc5817aa639e8a6a89fbb308
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-27 16:17:15 +0300

    populate hash from array

--- exercisec.rb
+++ exercisec.rb
@@ -65,3 +65,36 @@ hash1 = {:course => "software development"}
 hash2 = {course: "software development"}
 puts hash1
 puts hash2
+
+puts "\n"
+
+#9 Suppose you have a hash h = {a:1, b:2, c:3, d:4}
+
+h = {a:1, b:2, c:3, d:4}
+puts h[:b]
+h[:e] = 5
+
+h.delete_if{|k, v| v < 3.5}
+print h
+
+puts "\n"
+puts "\n"
+
+#10  Write a program that copies the information from the array into the empty hash that applies to the correct person.
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+
+contacts["Joe Smith"]
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:email] = contact_data[1][1]
+contacts["Sally Johnson"][:email] = contact_data[1][2]
+
+print contacts

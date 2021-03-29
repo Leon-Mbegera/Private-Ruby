5ddc6b947a1ea39ec41b38f3d8efcc1123db4259
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-03-29 18:48:27 +0300

    array containing nums in a range

--- exercisec.rb
+++ exercisec.rb
@@ -435,6 +435,25 @@ print even_nums(10) # => [0, 2, 4, 6, 8, 10]
 puts
 print even_nums(5)  # => [0, 2, 4]
 
+puts"\n"
+
+#28 return array containing numbers in range of min-max
+
+def range(min, max)
+  arr = []
+  i = min
+  while i <= max
+    arr << i
+
+    i += 1
+  end
+  return arr
+
+end
+
+print range(2, 7)   # => [2, 3, 4, 5, 6, 7]
+puts
+print range(13, 20) # => [13, 14, 15, 16, 17, 18, 19, 20]
 
 
 
